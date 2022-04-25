d653505dd3259d0af692fc6b94266bb8de8800dc
EmielEDW <90001168+EmielEDW@users.noreply.github.com>
2022-04-25 23:23:23 +0200

    Add files via upload

---
+++ HelloWorld/src/com/emieledw/Main.java
@@ -0,0 +1,17 @@
+/*
+    HelloWorld - 1st Program
+    ------------------------
+    This is my first Java program! Very easy lol.
+    ----------------------
+    Author: Emiel De Waele
+    Version: v1.0
+ */
+
+package com.emieledw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
